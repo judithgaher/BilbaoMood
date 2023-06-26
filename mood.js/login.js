4620cd4d4f34772eb4999051b8def91e2ac9aa65
Your Name <you@example.com>
2023-06-26 12:34:34 +0200

    lunes-prueba-responsive

--- mood.js/login.js
+++ mood.js/login.js
@@ -1,5 +1,5 @@
 
-var usuarios = [
+let usuarios = [
     { username: "usuario1", password: "contraseña1" },
     { username: "usuario2", password: "contraseña2" },
     { username: "usuario3", password: "contraseña3" }
